ae5605823684c576598359cfebdb39c370bfc6be
James Palmer-Moore (s5416624) <s5416624@bournemouth.ac.uk>
2022-11-11 12:30:22 +0000

    Basic setup of canvas and script connection to darkroom.html

---
+++ clickasnap_files/darkroom.js
@@ -0,0 +1,8 @@
+const mainCanvas = Document.getElementById("mainCanvas");
+mainCanvas.width = "500px";
+mainCanvas.height= "500px";
+
+const ctx = mainCanvas.getContext("2d"); 
+ctx.moveTo(0, 0);
+ctx.lineTo(200, 100);
+ctx.stroke();
