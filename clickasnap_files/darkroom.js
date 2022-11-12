c517b0493f428c4d2a94b957fd1ab256f4b252f5
0SHI <jimmypm0701@gmail.com>
2022-11-12 15:09:19 +0000

    Actual dark room, scrolling

--- clickasnap_files/darkroom.js
+++ clickasnap_files/darkroom.js
@@ -1,9 +1,26 @@
+/**
+ * Editors:
+ *      James Palmer-Moore
+ *      Joe Baker
+ * Created: 
+ *      11/11/2022
+ */
+
+
+/* Gloabals */
 const mainCanvas = document.getElementById("mainCanvas");
 const ctx = mainCanvas.getContext("2d");
-ctx.lineWidth = 2;
-ctx.strokeRect(0, 0, 5760, 1080);
+const canvasContainer = document.getElementsByClassName("Canvas-Container");
+const roomSize = 5760; // def: 1920
 
-function box(coordinates, fill) {
+
+/* Setup */
+ctx.lineWidth = 2;
+ctx.strokeRect(0, 0, roomSize, 1080);
+
+
+/* Room Functions */
+function shape(coordinates, fill) {
     ctx.beginPath();
     ctx.moveTo(coordinates[0][0], coordinates[0][1]);
     for (let i = 1; i < coordinates.length; i++) {
@@ -13,20 +30,43 @@ function box(coordinates, fill) {
     ctx.fillStyle = fill;
     ctx.fill();
 }
+shape([[200, 0], [(roomSize - 200), 0], [(roomSize - 200), 800], [200, 800]], "rgba(25, 25, 25, 0.5)"); // Back wall
+shape([[0, 0], [200, 0], [200, 800], [0, 1080]], "rgba(50, 50, 50, 0.5)"); // Left wall
+shape([[roomSize, 0], [(roomSize - 200), 0], [(roomSize - 200), 800], [roomSize, 1080]], "rgba(50, 50, 50, 0.5)"); // Right wall
+shape([[200, 800], [(roomSize - 200), 800], [roomSize, 1080], [0, 1080]], "rgba(20, 20, 20, 0.5)"); // Floor
 
-box([[200, 0], [5560, 0], [5560, 800], [200, 800]], "rgba(255, 0, 0, 0.5)"); // Back wall
-box([[0, 0], [200, 0], [200, 800], [0, 1080]], "rgba(0, 0, 255, 0.5)"); // Left wall
-box([[5760, 0], [5560, 0], [5560, 800], [5760, 1080]], "rgba(0, 0, 255, 0.5)"); // Right wall
-box([[200, 800], [5560, 800], [5760, 1080], [0, 1080]], "rgba(0, 255, 0, 0.5)"); // Floor
 
-
-function killMe(offset=0) {
-    box([[offset+500, 600], [offset+1420, 600], [offset+1570, 850], [offset+350, 850]], "rgba(100, 100, 100, 1)"); // Table Top
-    box([[offset+360, 875], [offset+410, 875], [offset+410, 1080], [offset+360, 1080]], "rgba(100, 100, 100, 1)"); // Table Leg 1
-    box([[offset+1510, 875], [offset+1560, 875], [offset+1560, 1080], [offset+1510, 1080]], "rgba(100, 100, 100, 1)"); // Table Leg 2
-    box([[offset+350, 850], [offset+1570, 850], [offset+1570, 875], [offset+350, 875]], "rgba(10, 10, 10, 1)"); // Table 3d/bottom
+function table(offset=0) {
+    shape([[offset+500, 600], [offset+1420, 600], [offset+1570, 850], [offset+350, 850]], "rgba(100, 100, 100, 1)"); // Table Top
+    shape([[offset+360, 875], [offset+410, 875], [offset+410, 1080], [offset+360, 1080]], "rgba(100, 100, 100, 1)"); // Table Leg 1
+    shape([[offset+1510, 875], [offset+1560, 875], [offset+1560, 1080], [offset+1510, 1080]], "rgba(100, 100, 100, 1)"); // Table Leg 2
+    shape([[offset+350, 850], [offset+1570, 850], [offset+1570, 875], [offset+350, 875]], "rgba(10, 10, 10, 1)"); // Table 3d/bottom
 }
 
-killMe();
-killMe(1920);
-killMe(1920*2);
+let noTables = roomSize / 1920;
+for (let i = 0; i < noTables; i++) table(1920*i);
+
+
+function scroll(event) {
+    let left = parseInt(canvasContainer[0].style.left.slice(0,-2));
+
+    if(left == 0 && event.deltaY > 0)
+        return;
+    if(left == -3800 && event.deltaY < 0)
+        return;
+
+    if(left>0) // Extra checks due to bug
+        canvasContainer[0].style.left = "0px";
+    else if (left<(1000-(roomSize-(1920)/2)))
+        canvasContainer[0].style.left = "-3700px";
+
+    else 
+        canvasContainer[0].style.left = (left+event.deltaY)+"px";
+}
+
+var mousewheelEvent=(/Firefox/i.test(navigator.userAgent))? "DOMMouseScroll" : "mousewheel";
+
+if(document.attachEvent)
+    document.attachEvent("on"+mousewheelEvent, function(e){scroll(e)});
+else if (document.addEventListener)
+    document.addEventListener(mousewheelEvent,  function(e){scroll(e)}, false);
